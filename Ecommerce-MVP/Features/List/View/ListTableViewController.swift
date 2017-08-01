3974b0c4e8ec7cbf6b94aa92f6de274fd86f6818
Lucas M Soares <lucas1991rj@gmail.com>
2017-07-31 22:32:33 -0300

    first commit

---
+++ Ecommerce-MVP/Features/List/View/ListTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ListTableViewController.swift
+//  Ecommerce-MVP
+//
+//  Created by Lucas M Soares on 30/07/17.
+//  Copyright © 2017 Lucas M Soares. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
---
+++ Ecommerce-MVP/PokemonCell.swift
@@ -0,0 +1,24 @@
+//
+//  PokemonCell.swift
+//  Ecommerce-MVP
+//
+//  Created by Lucas M Soares on 31/07/17.
+//  Copyright © 2017 Lucas M Soares. All rights reserved.
+//
+
+import UIKit
+
+class PokemonCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
